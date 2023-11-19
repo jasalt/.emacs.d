7d3e32ce26c3ea13df179b8365484e4a2332d642
Jarkko Saltiola <jarkko@saltiolacode.fi>
2023-11-19 20:16:57 +0200

    custom-set-variables update

--- init.el
+++ init.el
@@ -262,7 +262,7 @@ If the new path's directories does not exist, create them."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key)))
+ '(package-selected-packages '(evil which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
