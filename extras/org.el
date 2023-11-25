727d1e9666736e0e58d0fc7c60bc96b5fec49270
Jarkko Saltiola <jarkko@saltiolacode.fi>
2023-11-25 14:45:11 +0200

    Basic org-mode config

--- extras/org.el
+++ extras/org.el
@@ -41,30 +41,30 @@
 ;;; Phase 2 variables
 
 ;; Agenda variables
-(setq org-directory "~/Documents/org/") ; Non-absolute paths for agenda and
-                                        ; capture templates will look here.
+(setq org-directory "~/notes/") ; Non-absolute paths for agenda and
+					; capture templates will look here.
 
-(setq org-agenda-files '("inbox.org" "work.org"))
+(setq org-agenda-files '("inbox.org" "todo.org" "work.org"))
 
 ;; Default tags
 (setq org-tag-alist '(
-                      ;; locale
-                      (:startgroup)
-                      ("home" . ?h)
-                      ("work" . ?w)
-                      ("school" . ?s)
-                      (:endgroup)
-                      (:newline)
-                      ;; scale
-                      (:startgroup)
-                      ("one-shot" . ?o)
-                      ("project" . ?j)
-                      ("tiny" . ?t)
-                      (:endgroup)
-                      ;; misc
-                      ("meta")
-                      ("review")
-                      ("reading")))
+		      ;; locale
+		      (:startgroup)
+		      ("home" . ?h)
+		      ("work" . ?w)
+		      ("school" . ?s)
+		      (:endgroup)
+		      (:newline)
+		      ;; scale
+		      (:startgroup)
+		      ("one-shot" . ?o)
+		      ("project" . ?j)
+		      ("tiny" . ?t)
+		      (:endgroup)
+		      ;; misc
+		      ("meta")
+		      ("review")
+		      ("reading")))
 
 ;; Org-refile: where should org-refile look?
 (setq org-refile-targets 'FIXME)
@@ -72,16 +72,16 @@
 ;;; Phase 3 variables
 
 ;; Org-roam variables
-(setq org-roam-directory "~/Documents/org-roam/")
-(setq org-roam-index-file "~/Documents/org-roam/index.org")
+;; (setq org-roam-directory "~/Documents/org-roam/")
+;; (setq org-roam-index-file "~/Documents/org-roam/index.org")
 
 ;;; Optional variables
 
 ;; Advanced: Custom link types
 ;; This example is for linking a person's 7-character ID to their page on the
 ;; free genealogy website Family Search.
-(setq org-link-abbrev-alist
-      '(("family_search" . "https://www.familysearch.org/tree/person/details/%s")))
+;; (setq org-link-abbrev-alist
+;;       '(("family_search" . "https://www.familysearch.org/tree/person/details/%s")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -90,15 +90,19 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package org
-  :hook ((org-mode . visual-line-mode)  ; wrap lines at word breaks
-         (org-mode . flyspell-mode))    ; spell checking!
+  :hook ((org-mode . visual-line-mode) ; wrap lines at word breaks
+;	 (org-mode . org-indent-mode 1)
+	 (org-mode . flyspell-mode))    ; spell checking!
 
   :bind (:map global-map
-              ("C-c l s" . org-store-link)          ; Mnemonic: link → store
-              ("C-c l i" . org-insert-link-global)) ; Mnemonic: link → insert
+	      ("C-c l s" . org-store-link)          ; Mnemonic: link → store
+	      ("C-c l i" . org-insert-link-global)
+	      ("C-c 1" . (lambda () (interactive) (find-file (concat org-directory "inbox.org"))))
+
+	      ) ; Mnemonic: link → insert
   :config
-  (require 'oc-csl)                     ; citation support
-  (add-to-list 'org-export-backends 'md)
+  ;; (require 'oc-csl)                     ; citation support
+  ;; (add-to-list 'org-export-backends 'md)
 
   ;; Make org-open-at-point follow file links in the same window
   (setf (cdr (assoc 'file org-link-frame-setup)) 'find-file)
@@ -121,31 +125,35 @@
   ;; Instead of just two states (TODO, DONE) we set up a few different states
   ;; that a task can be in.
   (setq org-todo-keywords
-        '((sequence "TODO(t)" "WAITING(w@/!)" "STARTED(s!)" "|" "DONE(d!)" "OBSOLETE(o@)")))
+	'((sequence "TODO(t)" "WAITING(w@/!)" "STARTED(s!)" "|" "DONE(d!)" "OBSOLETE(o@)")))
 
   ;; Refile configuration
   (setq org-outline-path-complete-in-steps nil)
   (setq org-refile-use-outline-path 'file)
 
   (setq org-capture-templates
-        '(("c" "Default Capture" entry (file "inbox.org")
-           "* TODO %?\n%U\n%i")
-          ;; Capture and keep an org-link to the thing we're currently working with
-          ("r" "Capture with Reference" entry (file "inbox.org")
-           "* TODO %?\n%U\n%i\n%a")
-          ;; Define a section
-          ("w" "Work")
-          ("wm" "Work meeting" entry (file+headline "work.org" "Meetings")
-           "** TODO %?\n%U\n%i\n%a")
-          ("wr" "Work report" entry (file+headline "work.org" "Reports")
-           "** TODO %?\n%U\n%i\n%a")))
+	'(("c" "Default Capture" entry (file "inbox.org")
+	   "* TODO %?\n%U\n%i")
+	  ;; Capture and keep an org-link to the thing we're currently working with
+	  ("r" "Capture with Reference" entry (file "inbox.org")
+	   "* TODO %?\n%U\n%i\n%a")
+	  ;; Define a section
+	  ("w" "Work")
+	  ("wm" "Work meeting" entry (file+headline "work.org" "Meetings")
+	   "** TODO %?\n%U\n%i\n%a")
+	  ("wr" "Work report" entry (file+headline "work.org" "Reports")
+	   "** TODO %?\n%U\n%i\n%a")
+	  ("j" "Journal entry" entry (file+datetree
+				      (lambda () (concat org-directory "texts/meta/notetoself.org")))
+               "* %?\nEntered on %U\n  %i\n")
+	  ))
 
     (setq org-agenda-custom-commands
-          '(("n" "Agenda and All Todos"
-             ((agenda)
-              (todo)))
-            ("w" "Work" agenda ""
-             ((org-agenda-files '("work.org")))))))
+	  '(("n" "Agenda and All Todos"
+	     ((agenda)
+	      (todo)))
+	    ("w" "Work" agenda ""
+	     ((org-agenda-files '("work.org")))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -153,17 +161,17 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package org-roam
-  :ensure t
-  :config
-  (org-roam-db-autosync-mode)
-  ;; Dedicated side window for backlinks
-  (add-to-list 'display-buffer-alist
-               '("\\*org-roam\\*"
-                 (display-buffer-in-side-window)
-                 (side . right)
-                 (window-width . 0.4)
-                 (window-height . fit-window-to-buffer))))
+;; (use-package org-roam
+;;   :ensure t
+;;   :config
+;;   (org-roam-db-autosync-mode)
+;;   ;; Dedicated side window for backlinks
+;;   (add-to-list 'display-buffer-alist
+;;                '("\\*org-roam\\*"
+;;                  (display-buffer-in-side-window)
+;;                  (side . right)
+;;                  (window-width . 0.4)
+;;                  (window-height . fit-window-to-buffer))))
 
 ;; Pretty web interface for org-roam
 ;(use-package org-roam-ui
