54ace217be6e29699e65854236a0afc6be0f336f
Jarkko Saltiola <jarkko@saltiolacode.fi>
2023-11-25 14:12:24 +0200

    Indent

--- custom.el
+++ custom.el
@@ -18,14 +18,13 @@
   (interactive)
   (if (string-match "*scratch*" (buffer-name))
       (progn
-        (switch-to-buffer (get-buffer-create (concat (buffer-name) "*")))
-        (princ "Created a new scratch buffer."))
+	(switch-to-buffer (get-buffer-create (concat (buffer-name) "*")))
+	(princ "Created a new scratch buffer."))
     (progn (switch-to-buffer
-            (get-buffer-create "*scratch*")))))
+	    (get-buffer-create "*scratch*")))))
 
 (global-set-key (kbd "C-c <escape>") 'show-or-create-scratch)
 (global-set-key (kbd "C-c `") 'show-or-create-scratch)  ;; terminal workaround
-
 
 (use-package winum :ensure t :config (winum-mode)
   ;; https://github.com/deb0ch/emacs-winum
@@ -106,28 +105,28 @@
   (interactive "DEnv: ")
 
   (let* (;; file-truename and tramp-file-local-name ensure that neither `~' nor
-         ;; the Tramp prefix (e.g. "/ssh:my-host:") wind up in the final
-         ;; absolute directory path.
-         (venv-dir (tramp-file-local-name (file-truename virtualenv)))
+	 ;; the Tramp prefix (e.g. "/ssh:my-host:") wind up in the final
+	 ;; absolute directory path.
+	 (venv-dir (tramp-file-local-name (file-truename virtualenv)))
 
-         ;; Given something like /path/to/.venv/, this strips off the trailing `/'.
-         (venv-file-name (directory-file-name venv-dir))
+	 ;; Given something like /path/to/.venv/, this strips off the trailing `/'.
+	 (venv-file-name (directory-file-name venv-dir))
 
-         ;; Naming convention for venvPath matches the field for
-         ;; pyrightconfig.json.  `file-name-directory' gets us the parent path
-         ;; (one above .venv).
-         (venvPath (file-name-directory venv-file-name))
+	 ;; Naming convention for venvPath matches the field for
+	 ;; pyrightconfig.json.  `file-name-directory' gets us the parent path
+	 ;; (one above .venv).
+	 (venvPath (file-name-directory venv-file-name))
 
-         ;; Grabs just the `.venv' off the end of the venv-file-name.
-         (venv (file-name-base venv-file-name))
+	 ;; Grabs just the `.venv' off the end of the venv-file-name.
+	 (venv (file-name-base venv-file-name))
 
-         ;; Eglot demands that `pyrightconfig.json' is in the project root
-         ;; folder.
-         (base-dir (vc-git-root default-directory))
-         (out-file (expand-file-name "pyrightconfig.json" base-dir))
+	 ;; Eglot demands that `pyrightconfig.json' is in the project root
+	 ;; folder.
+	 (base-dir (vc-git-root default-directory))
+	 (out-file (expand-file-name "pyrightconfig.json" base-dir))
 
-         ;; Finally, get a string with the JSON payload.
-         (out-contents (json-encode (list :venvPath venvPath :venv venv))))
+	 ;; Finally, get a string with the JSON payload.
+	 (out-contents (json-encode (list :venvPath venvPath :venv venv))))
 
     ;; Emacs uses buffers for everything.  This creates a temp buffer, inserts
     ;; the JSON payload, then flushes that content to final `pyrightconfig.json'
@@ -142,17 +141,17 @@
   :ensure t
 ;;  :hook (prog-mode . copilot-mode)  ;; TODO hook to specific languages elisp-mode etc
   :bind (("C-c M-f" . copilot-complete)
-         :map copilot-completion-map
-         ("C-g" . 'copilot-clear-overlay)
-         ("M-p" . 'copilot-previous-completion)
-         ("M-n" . 'copilot-next-completion)
-         ("<tab>" . 'copilot-accept-completion)
-         ("M-f" . 'copilot-accept-completion-by-word)
-         ("M-<return>" . 'copilot-accept-completion-by-line)))
+	 :map copilot-completion-map
+	 ("C-g" . 'copilot-clear-overlay)
+	 ("M-p" . 'copilot-previous-completion)
+	 ("M-n" . 'copilot-next-completion)
+	 ("<tab>" . 'copilot-accept-completion)
+	 ("M-f" . 'copilot-accept-completion-by-word)
+	 ("M-<return>" . 'copilot-accept-completion-by-line)))
 
 (straight-use-package 'gptel)
 
-;;;; Treemacs 
+;;;; Treemacs
 ;; Full stock config from https://github.com/Alexander-Miller/treemacs#installation
 
 (use-package treemacs
@@ -164,57 +163,57 @@
   :config
   (progn
     (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay        0.5
-          treemacs-directory-name-transformer      #'identity
-          treemacs-display-in-side-window          t
-          treemacs-eldoc-display                   'simple
-          treemacs-file-event-delay                2000
-          treemacs-file-extension-regex            treemacs-last-period-regex-value
-          treemacs-file-follow-delay               0.2
-          treemacs-file-name-transformer           #'identity
-          treemacs-follow-after-init               t
-          treemacs-expand-after-init               t
-          treemacs-find-workspace-method           'find-for-file-or-pick-first
-          treemacs-git-command-pipe                ""
-          treemacs-goto-tag-strategy               'refetch-index
-          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
-          treemacs-hide-dot-git-directory          t
-          treemacs-indentation                     1
-          treemacs-indentation-string              " "
-          treemacs-is-never-other-window           nil
-          treemacs-max-git-entries                 5000
-          treemacs-missing-project-action          'ask
-          treemacs-move-forward-on-expand          nil
-          treemacs-no-png-images                   nil
-          treemacs-no-delete-other-windows         t
-          treemacs-project-follow-cleanup          nil
-          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                        'left
-          treemacs-read-string-input               'from-child-frame
-          treemacs-recenter-distance               0.1
-          treemacs-recenter-after-file-follow      nil
-          treemacs-recenter-after-tag-follow       nil
-          treemacs-recenter-after-project-jump     'always
-          treemacs-recenter-after-project-expand   'on-distance
-          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
-          treemacs-project-follow-into-home        nil
-          treemacs-show-cursor                     t
-          treemacs-show-hidden-files               t
-          treemacs-silent-filewatch                nil
-          treemacs-silent-refresh                  nil
-          treemacs-sorting                         'alphabetic-asc
-          treemacs-select-when-already-in-treemacs 'move-back
-          treemacs-space-between-root-nodes        t
-          treemacs-tag-follow-cleanup              t
-          treemacs-tag-follow-delay                1.5
-          treemacs-text-scale                      -1
-          treemacs-user-mode-line-format           nil
-          treemacs-user-header-line-format         nil
-          treemacs-wide-toggle-width               70
-          treemacs-width                           26
-          treemacs-width-increment                 1
-          treemacs-width-is-initially-locked       t
-          treemacs-workspace-switch-cleanup        nil)
+	  treemacs-deferred-git-apply-delay        0.5
+	  treemacs-directory-name-transformer      #'identity
+	  treemacs-display-in-side-window          t
+	  treemacs-eldoc-display                   'simple
+	  treemacs-file-event-delay                2000
+	  treemacs-file-extension-regex            treemacs-last-period-regex-value
+	  treemacs-file-follow-delay               0.2
+	  treemacs-file-name-transformer           #'identity
+	  treemacs-follow-after-init               t
+	  treemacs-expand-after-init               t
+	  treemacs-find-workspace-method           'find-for-file-or-pick-first
+	  treemacs-git-command-pipe                ""
+	  treemacs-goto-tag-strategy               'refetch-index
+	  treemacs-header-scroll-indicators        '(nil . "^^^^^^")
+	  treemacs-hide-dot-git-directory          t
+	  treemacs-indentation                     1
+	  treemacs-indentation-string              " "
+	  treemacs-is-never-other-window           nil
+	  treemacs-max-git-entries                 5000
+	  treemacs-missing-project-action          'ask
+	  treemacs-move-forward-on-expand          nil
+	  treemacs-no-png-images                   nil
+	  treemacs-no-delete-other-windows         t
+	  treemacs-project-follow-cleanup          nil
+	  treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+	  treemacs-position                        'left
+	  treemacs-read-string-input               'from-child-frame
+	  treemacs-recenter-distance               0.1
+	  treemacs-recenter-after-file-follow      nil
+	  treemacs-recenter-after-tag-follow       nil
+	  treemacs-recenter-after-project-jump     'always
+	  treemacs-recenter-after-project-expand   'on-distance
+	  treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+	  treemacs-project-follow-into-home        nil
+	  treemacs-show-cursor                     t
+	  treemacs-show-hidden-files               t
+	  treemacs-silent-filewatch                nil
+	  treemacs-silent-refresh                  nil
+	  treemacs-sorting                         'alphabetic-asc
+	  treemacs-select-when-already-in-treemacs 'move-back
+	  treemacs-space-between-root-nodes        t
+	  treemacs-tag-follow-cleanup              t
+	  treemacs-tag-follow-delay                1.5
+	  treemacs-text-scale                      -1
+	  treemacs-user-mode-line-format           nil
+	  treemacs-user-header-line-format         nil
+	  treemacs-wide-toggle-width               70
+	  treemacs-width                           26
+	  treemacs-width-increment                 1
+	  treemacs-width-is-initially-locked       t
+	  treemacs-workspace-switch-cleanup        nil)
 
     ;; The default width and height of the icons is 22 pixels. If you are
     ;; using a Hi-DPI display, uncomment this to double the icon size.
@@ -227,7 +226,7 @@
       (treemacs-git-commit-diff-mode t))
 
     (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
+		 (not (null treemacs-python-executable)))
       (`(t . t)
        (treemacs-git-mode 'deferred))
       (`(t . _)
@@ -236,13 +235,13 @@
     (treemacs-hide-gitignored-files-mode nil))
   :bind
   (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t d"   . treemacs-select-directory)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
+	("M-0"       . treemacs-select-window)
+	("C-x t 1"   . treemacs-delete-other-windows)
+	("C-x t t"   . treemacs)
+	("C-x t d"   . treemacs-select-directory)
+	("C-x t B"   . treemacs-bookmark)
+	("C-x t C-t" . treemacs-find-file)
+	("C-x t M-t" . treemacs-find-tag)))
 
 (use-package treemacs-evil
   :after (treemacs evil)
