7cd1acdc49615ec7f46de3a10111b0e07bc3f889
Jarkko Saltiola <jarkko@saltiolacode.fi>
2024-01-23 10:25:32 +0200

    Set recentf cleanup after 5 minutes idle.
    Prelude disabled completely? Ended with old deleted files in recentf list.

--- custom.el
+++ custom.el
@@ -82,7 +82,7 @@
       recentf-max-menu-items 15
       ;; disable recentf-cleanup on Emacs start, because it can cause
       ;; problems with remote files
-      recentf-auto-cleanup 'never)
+      recentf-auto-cleanup 300) ; 'never
 (defun prelude-recentf-exclude-p (file)
   "A predicate to decide whether to exclude FILE from recentf."
   (let ((file-dir (file-truename (file-name-directory file))))
