0ebd38d92a748ce6acfa955822e0bd3838316577
Jarkko Saltiola <jarkko@saltiolacode.fi>
2024-02-13 09:53:18 +0200

    Disable embark (bedrock upstream default)
    which-key is enough

--- extras/base.el
+++ extras/base.el
@@ -65,28 +65,29 @@
   ;; Narrowing lets you restrict results to certain groups of candidates
   (setq consult-narrow-key "<"))
 
-(use-package embark
-  :ensure t
-  :demand t
-  :after avy
-  :bind (("C-c a" . embark-act))        ; bind this to an easy key to hit
-  :init
-  ;; Add the option to run embark when using avy
-  (defun bedrock/avy-action-embark (pt)
-    (unwind-protect
-        (save-excursion
-          (goto-char pt)
-          (embark-act))
-      (select-window
-       (cdr (ring-ref avy-ring 0))))
-    t)
 
-  ;; After invoking avy-goto-char-timer, hit "." to run embark at the next
-  ;; candidate you select
-  (setf (alist-get ?. avy-dispatch-alist) 'bedrock/avy-action-embark))
+;; (use-package embark ; Disabled default UI helper
+;;   :ensure t
+;;   :demand t
+;;   :after avy
+;;   :bind (("C-c a" . embark-act))        ; bind this to an easy key to hit
+;;   :init
+;;   ;; Add the option to run embark when using avy
+;;   (defun bedrock/avy-action-embark (pt)
+;;     (unwind-protect
+;;         (save-excursion
+;;           (goto-char pt)
+;;           (embark-act))
+;;       (select-window
+;;        (cdr (ring-ref avy-ring 0))))
+;;     t)
 
-(use-package embark-consult
-  :ensure t)
+;;   ;; After invoking avy-goto-char-timer, hit "." to run embark at the next
+;;   ;; candidate you select
+;;   (setf (alist-get ?. avy-dispatch-alist) 'bedrock/avy-action-embark))
+
+;; (use-package embark-consult
+;;   :ensure t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
